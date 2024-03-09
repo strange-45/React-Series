03d98a185af91e874628d59e9c9e950820c04652
Abhay-raj69 <rajabhay56999@gmail.com>
2024-03-09 18:03:12 +0530

    changes added

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,9 @@
+
+import Chai from "./chai"
 function App() {
 
   return (
-    <h1>React Series With Vite</h1>
+    <Chai/>
   )
 }
 
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+      <h3>Code is Ready</h3>  
+    )
+}
+
+export default Chai
