409cbb76d11dba5677377c59f5e66e288d6a6335
Abhay-raj69 <rajabhay56999@gmail.com>
2024-03-15 03:20:24 +0530

    some previous changes that were not pushed

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,10 @@
-
-import Chai from "./chai"
+import Chai from "./chai";
 function App() {
-
   return (
-    <Chai/>
-  )
+    <>
+      <Chai />
+      <h1>React Padh rhe hai dosto</h1>
+    </>
+  );
 }
-
-export default App
+export default App;
